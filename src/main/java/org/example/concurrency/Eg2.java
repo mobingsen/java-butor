7c5679152ea038529a0dc40ac9ea462c5152208e
mbs <2740303622@qq.com>
2020-06-28 10:35:37 +0800

    eg2

--- src/main/java/org/example/concurrency/Eg2.java
+++ src/main/java/org/example/concurrency/Eg2.java
@@ -12,55 +12,79 @@ public class Eg2 {
 
     public static void main(String[] args) {
         Counter counter = new Counter();
-        new Thread(() -> {
-            while (true) {
-                counter.increase();
-            }
-        }).start();
-        new Thread(() -> {
-            while (true) {
-                counter.decrease();
-            }
-        }).start();
+        new IncreaseThread(counter).start();
+        new DecreaseThread(counter).start();
+        // 直接运行以上的increase1和decrease1线程，结果是正确的的，但是如果分别对increase1和decrease1线程再增加多个线程进行处理就会
+        // 出现了意料之外的错误这是因为condition的条件判断使用了if，而在object的wait方法中说明使用while。
+        // 同一个condition时存在多个线程并发时，必须使用notifyAll才行，使用notify方法会导致满足条件的线程无法唤醒。
+        new IncreaseThread(counter).start();
+        new DecreaseThread(counter).start();
+    }
+}
+
+class IncreaseThread extends Thread {
+
+    private final Counter counter;
+
+    IncreaseThread(Counter counter) {
+        this.counter = counter;
     }
 
-    static class Counter {
-        private int counter;
+    @Override
+    public void run() {
+        for (int i = 0; i < 20; i++) {
+            counter.increase();
+        }
+    }
+}
 
-        public synchronized void increase() {
-            if (counter != 0) {
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            counter++;
-            System.out.println(counter);
+class DecreaseThread extends Thread {
+
+    private final Counter counter;
+
+    DecreaseThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 20; i++) {
+            counter.decrease();
+        }
+    }
+}
+
+class Counter {
+
+    private int counter;
+
+    public synchronized void increase() {
+//            if (counter != 0) {
+        while (counter != 0) {
             try {
-                Thread.sleep(500);
+                System.out.println("--" + counter);
+                wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            notify();
         }
+        counter++;
+        System.out.println(counter);
+        notifyAll();
+    }
 
-        public synchronized void decrease() {
-            if (counter != 1) {
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            counter--;
-            System.out.println(counter);
+    public synchronized void decrease() {
+//            if (counter != 1) {
+        while (counter != 1) {
             try {
-                Thread.sleep(500);
+                System.out.println("--" + counter);
+                wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            notify();
         }
+        counter--;
+        System.out.println(counter);
+        notifyAll();
     }
 }
