5a66ca02da34335bed1d41062d23d57945a729eb
mbs <2740303622@qq.com>
2020-07-20 10:58:46 +0800

    eg

---
+++ src/main/java/org/example/concurrency/Eg25.java
@@ -0,0 +1,49 @@
+package org.example.concurrency;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * Created by mbs on 2020/7/15 16:03
+ */
+public class Eg25 {
+
+    public static void main(String[] args) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool(2);
+        EgTask egTask = new EgTask(1, 10);
+        Integer result = forkJoinPool.invoke(egTask);
+        System.out.println(result);
+    }
+}
+
+class EgTask extends RecursiveTask<Integer> {
+
+    private int limit = 4;
+    private int firstIndex;
+    private int lastIndex;
+
+    public EgTask(int firstIndex, int lastIndex) {
+        this.firstIndex = firstIndex;
+        this.lastIndex = lastIndex;
+    }
+
+    @Override
+    protected Integer compute() {
+        int result = 0;
+        int gap = lastIndex - firstIndex;
+        boolean flag = gap <= limit;
+        if (flag) {
+            System.out.println(Thread.currentThread().getName());
+            for (int i = firstIndex; i <= lastIndex; i++) {
+                result += i;
+            }
+        } else {
+            int middleIndex = (firstIndex + lastIndex) / 2;
+            EgTask left = new EgTask(firstIndex, middleIndex);
+            EgTask right = new EgTask(middleIndex + 1, lastIndex);
+            invokeAll(left, right);
+            result = left.join() + right.join();
+        }
+        return result;
+    }
+}
---
+++ src/main/java/org/example/concurrency/Eg26.java
@@ -0,0 +1,32 @@
+package org.example.concurrency;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+/**
+ * Created by mbs on 2020/7/15 17:10
+ */
+public class Eg26 {
+
+    public static void main(String[] args) throws Exception {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(4, 10, 10,
+                TimeUnit.SECONDS, new LinkedBlockingQueue<>(), new ThreadPoolExecutor.AbortPolicy());
+        CompletionService<Integer> completionService = new ExecutorCompletionService<>(executor);
+        IntStream.range(0, 10).boxed().map(i -> (Callable<Integer>) () -> {
+            TimeUnit.MILLISECONDS.sleep((long) (Math.random() * 1000));
+            System.out.println(Thread.currentThread().getName());
+            return i * i;
+        }).forEach(completionService::submit);
+        for (int i = 0; i < 10; i++) {
+            Future<Integer> future = completionService.take();
+            System.out.println(future.get());
+        }
+        executor.shutdown();
+    }
+}
