f341cbf1c977d66591786fe4f51b6f8dcfe842e1
mbs <2740303622@qq.com>
2020-10-17 17:01:46 +0800

    hj

---
+++ src/main/java/org/example/hj/_1_ReverseWord.java
@@ -0,0 +1,29 @@
+package org.example.hj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class _1_ReverseWord {
+
+    /**
+     * ready go
+     * ydaer og
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = reader.readLine()) != null && line.replaceAll(" ", "").length() > 0) {
+            String[] split = line.split(" ");
+            String result = Arrays.stream(split)
+                    .map(StringBuilder::new)
+                    .map(StringBuilder::reverse)
+                    .collect(Collectors.joining(" "));
+            System.out.println(result);
+        }
+    }
+}
