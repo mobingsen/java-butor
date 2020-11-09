dfc8086e6c79b2a2dd97afff2dd4aa00d9d01f39
小墨 <2740303622@qq.com>
2020-11-09 18:55:50 +0800

    reactor guava

---
+++ src/main/java/org/example/_21_StrongReference.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class _21_StrongReference {
+}
---
+++ src/main/java/org/example/_27_Mod.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class _27_Mod {
+
+    private static final int BASE = 0x7;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < BASE; i++) {
+            System.out.println((i & BASE) + "\t" + (i % BASE));
+        }
+    }
+}
