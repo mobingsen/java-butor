5a1521e66867b5ae0b625011a8fe5938b88cc622
2740303622@qq.com <mobingsen>
2021-01-01 13:21:35 +0800

    leetcode

--- src/main/java/org/example/leetcode/_1631_MinimumEffortPath.java
+++ src/main/java/org/example/leetcode/_1631_MinimumEffortPath.java
@@ -1,11 +1,9 @@
 package org.example.leetcode;
 
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 /**
- * Created by mbs on 2020/12/24 11:40
+ * https://leetcode-cn.com/problems/path-with-minimum-effort/
  */
 public class _1631_MinimumEffortPath {
 
@@ -17,34 +15,30 @@ public class _1631_MinimumEffortPath {
     }
 
     public int minimumEffortPath(int[][] heights) {
-
-
         int m = heights.length, n = heights[0].length;
         int[][] dist = new int[m][n];
         int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        for (int[] d : dist) {
+        for(int[] d : dist)
             Arrays.fill(d, Integer.MAX_VALUE);
-        }
         dist[0][0] = 0;
-        Queue<int[]> queue = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        Queue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a[2]));
         boolean[][] visited = new boolean[m][n];
         queue.offer(new int[]{0, 0, 0});
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()) {
             int[] cur = queue.poll();
             int x = cur[0], y = cur[1];
-            if (visited[x][y]) {
+            if(visited[x][y])
                 continue;
-            }
             visited[x][y] = true;
-            for (int[] d : dir) {
+            for(int[] d : dir) {
                 int nx = x + d[0], ny = y + d[1];
-                if (0 <= nx && nx < m && 0 <= ny && ny < n) {
+                if(0 <= nx && nx < m && 0 <= ny && ny < n) {
                     int effort = Math.max(dist[x][y], Math.abs(heights[x][y] - heights[nx][ny]));
                     dist[nx][ny] = Math.min(dist[nx][ny], effort);
                     queue.offer(new int[]{nx, ny, dist[nx][ny]});
                 }
             }
         }
-        return dist[m - 1][n - 1];
+        return dist[m-1][n-1];
     }
 }
