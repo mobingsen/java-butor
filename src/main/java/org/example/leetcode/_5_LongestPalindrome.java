72fc51ead5643e4344c485dbf4fe1e43ce1a275e
2740303622@qq.com <mobingsen>
2021-01-12 23:37:42 +0800

    leetcode

---
+++ src/main/java/org/example/leetcode/_5_LongestPalindrome.java
@@ -0,0 +1,17 @@
+package org.example.leetcode;
+
+/**
+ * https://leetcode-cn.com/problems/longest-palindromic-substring/
+ * Created by modou on 2021/1/12 21:50
+ */
+public class _5_LongestPalindrome {
+
+    public static void main(String[] args) {
+        _5_LongestPalindrome palindrome = new _5_LongestPalindrome();
+        System.out.println(palindrome.longestPalindrome(""));
+    }
+
+    public String longestPalindrome(String s) {
+        return null;
+    }
+}
