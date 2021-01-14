b9ed8708259ce4f127fff0bf72dd3106cb5ea5fd
2740303622@qq.com <mobingsen>
2021-01-14 22:08:21 +0800

    leetcode

--- src/main/java/org/example/leetcode/_5_LongestPalindrome.java
+++ src/main/java/org/example/leetcode/_5_LongestPalindrome.java
@@ -1,7 +1,5 @@
 package org.example.leetcode;
 
-import java.util.Stack;
-
 /**
  * https://leetcode-cn.com/problems/longest-palindromic-substring/
  * Created by modou on 2021/1/12 21:50
@@ -10,23 +8,40 @@ public class _5_LongestPalindrome {
 
     public static void main(String[] args) {
         _5_LongestPalindrome palindrome = new _5_LongestPalindrome();
-        System.out.println(palindrome.longestPalindrome(""));
+        System.out.println(palindrome.longestPalindrome("babad"));
+        System.out.println(palindrome.longestPalindrome("cbbd"));
+        System.out.println(palindrome.longestPalindrome("a"));
+        System.out.println(palindrome.longestPalindrome("ac"));
     }
 
     public String longestPalindrome(String s) {
-        if (s.length() == 1) {
+        int length = s.length();
+        if (length == 1) {
             return s;
         }
-        if (s.length() == 2) {
-            String[] split = s.split("");
-            return split[0].equals(split[1]) ? s : split[0];
+        if (length == 2) {
+            return s.charAt(0) == s.charAt(1) ? s : s.substring(0, 1);
         }
-        Stack<String> stack = new Stack<>();
-        String[] strings = s.split("");
-        for (String string : strings) {
-            boolean eq = stack.peek().equals(string);
-
+        int len = 1;
+        int start = 0;
+        boolean[][] dp = new boolean[length][length];
+        for (int j = 0; j < length; j++) {
+            for (int i = 0; i < j; i++) {
+                if (s.charAt(i) != s.charAt(j)) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else  {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+                if (dp[i][j] && j - i + 1 > len) {
+                    len = j - i + 1;
+                    start = i;
+                }
+            }
         }
-        return null;
+        return s.substring(start, start + len);
     }
 }
