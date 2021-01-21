47fc96b8788c0ae6a9e126d3cc3b7c078e7806e1
mobingsen <2740303622@qq.com>
2021-01-21 16:49:01 +0800

    java

---
+++ src/main/java/org/example/_35_Float.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Created by mbs on 2021/1/21 10:30
+ */
+public class _35_Float {
+
+    public static void main(String[] args) {
+        int one = 10;
+        int three = 3;
+        float result = one * 1.00f / three;
+        System.out.println(result);
+        System.out.println(Math.round(result * 100) / 100f);
+    }
+}
