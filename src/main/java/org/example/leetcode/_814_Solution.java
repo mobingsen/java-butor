7b75be21a6d4aa4bb2b5c3652773b9894900078f
mobingsen <mobingsen@xiao100.com>
2021-06-11 10:06:39 +0800

    leetcode

---
+++ src/main/java/org/example/leetcode/_814_Solution.java
@@ -0,0 +1,25 @@
+package org.example.leetcode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class _814_Solution {
+
+    public TreeNode pruneTree(TreeNode root) {
+        return null;
+    }
+
+
+}
