8e6b1efa67e0b8f460a1f41ae528e780f4c10502
2740303622@qq.com <mobingsen>
2021-06-24 20:53:19 +0800

    leetcode

---
+++ src/main/java/org/example/leetcode/_42_Trap.java
@@ -0,0 +1,29 @@
+package org.example.leetcode;
+
+/**
+ * @author by mobingsen on 2021/6/24 20:44
+ */
+public class _42_Trap {
+
+    public int trap(int[] height) {
+        int ans = 0;
+        int size = height.length;
+        for (int i = 1; i < size - 1; i++) {
+            int maxLeft = 0;
+            int maxRight = 0;
+            for (int j = i; j >= 0; j--) {
+                maxLeft = Math.max(maxLeft, height[j]);
+            }
+            for (int j = i; j < size; j++) {
+                maxRight = Math.max(maxRight, height[j]);
+            }
+            ans += Math.min(maxLeft, maxRight) - height[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        _42_Trap trap = new _42_Trap();
+        System.out.println(trap.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+    }
+}
