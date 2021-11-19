eb99b98a8eec13513ceea725c4bc0e699df67dc9
2740303622@qq.com <mobingsen>
2021-11-19 23:54:52 +0800

    greedy

---
+++ src/main/java/org/example/algorithms/code07/BestArrange.java
@@ -0,0 +1,30 @@
+package org.example.algorithms.code07;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 贪心算法
+ * @author mobingsen
+ * @date 2021/11/19 23:10
+ */
+public class BestArrange {
+
+    public static void main(String[] args) {
+        int num = bestArrange(new Program[]{}, 6);
+        System.err.println(num);
+    }
+
+    private static int bestArrange(Program[] programs, int timePoint) {
+        Arrays.sort(programs, Comparator.comparingInt(Program::getEnd));
+        int result = 0;
+        // 从左往右遍历所有的会议
+        for (Program program : programs) {
+            if (timePoint <= program.getStart()) {
+                result++;
+                timePoint = program.getEnd();
+            }
+        }
+        return result;
+    }
+}
