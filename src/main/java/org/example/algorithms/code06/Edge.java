ca480fcf64846cf2c369e8d8d3267b8b4d9ca4a7
2740303622@qq.com <mobingsen>
2021-11-26 00:08:55 +0800

    code8

---
+++ src/main/java/org/example/algorithms/code06/Edge.java
@@ -0,0 +1,36 @@
+package org.example.algorithms.code06;
+
+/**
+ * @author mobingsen
+ * @date 2021/11/21 8:49
+ */
+public class Edge {
+
+    public Node from;
+    public Node to;
+    public int weight;
+
+    public Node getFrom() {
+        return from;
+    }
+
+    public void setFrom(Node from) {
+        this.from = from;
+    }
+
+    public Node getTo() {
+        return to;
+    }
+
+    public void setTo(Node to) {
+        this.to = to;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
---
+++ src/main/java/org/example/algorithms/code08/Hanoi.java
@@ -0,0 +1,30 @@
+package org.example.algorithms.code08;
+
+/**
+ * 汉诺塔问题
+ * @author mobingsen
+ * @date 2021/11/25 23:10
+ */
+public class Hanoi {
+
+    public static void main(String[] args) {
+        int n = 3;
+        hanoi(n);
+    }
+
+    private static void hanoi(int n) {
+        if (n > 0) {
+            func(n, "左", "右", "中");
+        }
+    }
+
+    private static void func(int i, String from, String to, String other) {
+        if (i == 1) {
+            System.out.println("Move 1 from " + from + " to " + to);
+        } else {
+            func(i - 1, from, other, to);
+            System.out.println("Move " + i + " from " + from + " to " + to);
+            func(i - 1, other, to, from);
+        }
+    }
+}
